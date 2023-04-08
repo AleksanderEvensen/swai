41eb6b7eb0923b55b25c4162bf7dbb25cb1e54f5
Aleksander Evensen <ahse03@gmail.com>
2023-04-08 14:18:06 +0200

    Moved to a workspace setup

--- src/main.rs
+++ src/main.rs
@@ -4,33 +4,70 @@ use nom::{IResult, InputIter};
 
 fn main() {
     let mut add_file = File::open("./tests/memory.wasm").unwrap();
-    let data = swai::parse(&mut add_file).unwrap();
+    let data = swai_parser::parse(&mut add_file).unwrap();
 
     println!("{:#?}", data);
 
-    let input: &[u8] = &[0xBF, 0x80, 0x80, 0x80, 0x80];
+    let input: &[u8] = &[0xBF, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x30];
 
-    println!("{:?}", test(input));
+    let input2: &[u8] = &[0x80, 0x80, 0x40];
 
-    println!("{}", leb128_u64(input));
+    println!("Unsigned: {:?}", test(input));
+    println!("Signed: {:?}", test2(input2));
+    println!("---");
+
+    println!("Unsigned: {:?}", leb128_u64(input));
+    println!("Signed: {:?}", leb128_i64(input2));
 }
 
 fn test(input: &[u8]) -> IResult<&[u8], u64> {
     nom_leb128::leb128_u64(input)
 }
+fn test2(input: &[u8]) -> IResult<&[u8], i64> {
+    nom_leb128::leb128_i64(input)
+}
 
-fn leb128_u64(input: &[u8]) -> u64 {
+fn leb128_u64(input: &[u8]) -> Result<u64, String> {
     let mut result: u64 = 0;
     let mut shift: u64 = 0;
 
-    for (_, byte) in input.iter_indices() {
+    for (i, byte) in input.iter_indices() {
         result |= ((byte & 0x7F) as u64) << shift;
 
         if (byte & 0x80) == 0 {
-            break;
+            return Ok(result);
+        } else if i >= leb128_size::<u64>() - 1 {
+            return Err("Number has is too large (Integer overflow)".to_string());
         }
         shift += 7;
     }
+    Err(format!("Incomplete byte array"))
+}
 
-    return result;
+fn leb128_i64(input: &[u8]) -> Result<i64, String> {
+    let mut result = 0;
+    let mut shift = 0;
+    let size = core::mem::size_of::<i64>() * 8;
+
+    for (i, byte) in input.iter_indices() {
+        result |= ((byte & 0x7F) as i64) << shift;
+        shift += 7;
+
+        if (byte & 0x80) == 0 {
+            if (shift < size) && (byte & 0x40) != 0 {
+                result |= !0 << shift;
+            }
+
+            return Ok(result);
+        } else if i >= leb128_size::<u64>() - 1 {
+            return Err("Number has is too large (Integer overflow)".to_string());
+        }
+    }
+
+    Err(format!("Incomplete byte array"))
+}
+
+fn leb128_size<T>() -> usize {
+    let bits = core::mem::size_of::<T>() * 8;
+    (bits + 6) / 7
 }
