4af2acc3ded56ec2b0cffc477a4a0194a82519f7
Aleksander Evensen <ahse03@gmail.com>
2023-04-09 00:27:40 +0200

    Added bytereader from the jvm project

--- bytereader/src/lib.rs
+++ bytereader/src/lib.rs
@@ -1,14 +1,248 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{fs::File, io::Read, string::FromUtf8Error};
+
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum ByteReaderError {
+    #[error(
+        "Attempted to read out of bounds of byte buffer (len: {length}) at offset: ({start}:{end})"
+    )]
+    OutOfBounds {
+        length: usize,
+        start: usize,
+        end: usize,
+    },
+    #[error("Sequence ({sequence:?}) wasn't found in the byte buffer")]
+    NotFound { sequence: Vec<u8> },
+
+    #[error("Failed to cast  type '{from}' to '{to}'")]
+    FailedTypeCast { from: String, to: String },
+
+    #[error("Failed to parse Utf8String from bytes")]
+    Utf8Error(#[from] FromUtf8Error),
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Clone, Copy)]
+pub enum Endian {
+    Little,
+    Big,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Clone)]
+pub struct ByteReader {
+    /// The byte buyffer
+    data: Vec<u8>,
+    /// The current offset (position) in the buffer
+    offset: usize,
+    /// Endian to use when reading for reading numbers
+    endian: Endian,
+    /// If the push_index function has been ran, the Option contains a value at that position, and can be returned to this position later using the pop_index function
+    push_offset: Option<usize>,
+}
+
+impl ByteReader {
+    pub fn from_vec(vec: &Vec<u8>) -> Self {
+        Self {
+            data: vec.to_vec(),
+            offset: 0,
+            endian: Endian::Little,
+            push_offset: None,
+        }
+    }
+
+    pub fn from_file(file: &mut File) -> std::io::Result<Self> {
+        let mut data = vec![];
+        file.read_to_end(&mut data)?;
+        Ok(Self {
+            data,
+            offset: 0,
+            endian: Endian::Little,
+            push_offset: None,
+        })
     }
 }
+
+impl ByteReader {
+    pub fn set_endian<'a>(&'a mut self, endian: Endian) -> &'a mut Self {
+        self.endian = endian;
+        self
+    }
+    pub fn move_to<'a>(&'a mut self, offset: usize) -> &'a mut Self {
+        self.offset = offset;
+        self
+    }
+
+    pub fn jump<'a>(&'a mut self, jump_by: usize) -> &'a mut Self {
+        self.offset += jump_by;
+        self
+    }
+
+    pub fn push_index<'a>(&'a mut self) -> &'a mut Self {
+        self.push_offset = Some(self.offset);
+        self
+    }
+    pub fn pop_index<'a>(&'a mut self) -> &'a mut Self {
+        if let Some(offset) = self.push_offset {
+            self.offset = offset;
+            self.push_offset = None;
+        }
+        self
+    }
+
+    pub fn get_file_length(&self) -> usize {
+        self.data.len()
+    }
+
+    pub fn get_current_offset(&self) -> usize {
+        self.offset
+    }
+    pub fn read_bytes(&mut self, bytes: usize) -> Result<&[u8], ByteReaderError> {
+        let data = self.peak_bytes(bytes)?;
+        self.offset += bytes;
+        Ok(data)
+    }
+    pub fn peak_bytes<'a>(&'a self, bytes: usize) -> Result<&'a [u8], ByteReaderError> {
+        if self.offset + bytes > self.data.len() {
+            return Err(ByteReaderError::OutOfBounds {
+                length: self.data.len(),
+                start: self.offset,
+                end: self.offset + bytes,
+            });
+        }
+
+        let data = self
+            .data
+            .get(self.offset..self.offset + bytes)
+            .ok_or_else(|| ByteReaderError::OutOfBounds {
+                length: self.data.len(),
+                start: self.offset,
+                end: self.offset + bytes,
+            })?;
+        Ok(data)
+    }
+
+    pub fn read_rest(&mut self) -> Result<&[u8], ByteReaderError> {
+        self.read_bytes(self.data.len() - self.offset)
+    }
+
+    pub fn peak_rest(&self) -> Result<&[u8], ByteReaderError> {
+        self.peak_bytes(self.data.len() - self.offset)
+    }
+
+    pub fn find_next(&self, sequence: &[u8]) -> Result<usize, ByteReaderError> {
+        self.find_from(sequence, self.offset.clone())
+    }
+    pub fn find(&self, sequence: &[u8]) -> Result<usize, ByteReaderError> {
+        self.find_from(sequence, 0)
+    }
+
+    pub fn find_all_offsets(&self, sequence: &Vec<u8>) -> Vec<usize> {
+        self.find_all_offsets_after(0, sequence)
+    }
+
+    pub fn find_all_offsets_after(&self, start_offset: usize, sequence: &Vec<u8>) -> Vec<usize> {
+        let mut offset = start_offset;
+        let mut found_offsets = vec![];
+        while let Ok(found_offset) = self.find_from(sequence, offset) {
+            found_offsets.push(found_offset);
+
+            offset = found_offset + 1
+        }
+        found_offsets
+    }
+
+    pub fn find_from(&self, sequence: &[u8], mut offset: usize) -> Result<usize, ByteReaderError> {
+        let bytes = sequence.len();
+
+        'data_loop: while let Some(data) = self.data.get(offset..offset + bytes) {
+            for (i, byte) in data.iter().enumerate() {
+                if sequence[i] != *byte {
+                    offset += 1;
+                    continue 'data_loop;
+                }
+            }
+            return Ok(offset);
+        }
+
+        return Err(ByteReaderError::NotFound {
+            sequence: sequence.to_vec(),
+        });
+    }
+    pub fn read_string_length<T: FromBinaryReader + Into<usize>>(
+        &mut self,
+    ) -> Result<String, ByteReaderError> {
+        let length = self.read::<T>()?;
+        self.read_string(length.into())
+    }
+    pub fn read_string_lossy_length<T: FromBinaryReader + Into<usize>>(
+        &mut self,
+    ) -> Result<String, ByteReaderError> {
+        let length = self.read::<T>()?;
+        self.read_string_lossy(length.into())
+    }
+
+    pub fn read_string(&mut self, length: usize) -> Result<String, ByteReaderError> {
+        Ok(String::from_utf8(self.read_bytes(length)?.to_vec())
+            .map_err(|err| ByteReaderError::Utf8Error(err))?)
+    }
+
+    pub fn read_string_lossy(&mut self, length: usize) -> Result<String, ByteReaderError> {
+        Ok(String::from_utf8_lossy(self.read_bytes(length)?).to_string())
+    }
+
+    pub fn read<T: FromBinaryReader>(&mut self) -> Result<T, ByteReaderError> {
+        T::read_from_byte_reader(self)
+    }
+
+    pub fn peak<T: FromBinaryReader>(&mut self) -> Result<T, ByteReaderError> {
+        T::peak_from_byte_reader(self)
+    }
+}
+
+pub trait FromBinaryReader {
+    fn read_from_byte_reader(reader: &mut ByteReader) -> Result<Self, ByteReaderError>
+    where
+        Self: Sized,
+    {
+        let v = Self::peak_from_byte_reader(reader)?;
+        reader.offset += std::mem::size_of::<Self>();
+
+        return Ok(v);
+    }
+
+    fn peak_from_byte_reader(reader: &ByteReader) -> Result<Self, ByteReaderError>
+    where
+        Self: Sized;
+}
+
+macro_rules! impl_from_binary_reader {
+    ($ty:ident) => {
+        impl FromBinaryReader for $ty {
+            fn peak_from_byte_reader(reader: &crate::ByteReader) -> Result<$ty, ByteReaderError> {
+                let bytes = std::mem::size_of::<Self>();
+                let data: [u8; std::mem::size_of::<Self>()] = reader
+                    .peak_bytes(bytes)?
+                    .try_into()
+                    .map_err(|_| ByteReaderError::FailedTypeCast {
+                        from: format!("&[{}]", stringify!($ty)),
+                        to: format!("[{}; {}]", stringify!($ty), bytes),
+                    })?;
+                match reader.endian {
+                    Endian::Little => Ok($ty::from_le_bytes(data)),
+                    Endian::Big => Ok($ty::from_be_bytes(data)),
+                }
+            }
+        }
+    };
+}
+
+impl_from_binary_reader!(u8);
+impl_from_binary_reader!(i8);
+impl_from_binary_reader!(u16);
+impl_from_binary_reader!(i16);
+impl_from_binary_reader!(u32);
+impl_from_binary_reader!(i32);
+impl_from_binary_reader!(u64);
+impl_from_binary_reader!(i64);
+impl_from_binary_reader!(f32);
+impl_from_binary_reader!(f64);
